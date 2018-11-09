1fe42911bf5b118679a4e27f486803c5797c3ed8
alireza rahi <alireza.rahi@outlook.com>
2018-11-09 19:48:35 +0330

    first commit

---
+++ src/org/studyeasy/vehicles/Bike.java
@@ -0,0 +1,24 @@
+package org.studyeasy.vehicles;
+
+import org.studyeasy.parent.Vehicle;
+
+public class Bike extends Vehicle {
+	private String handle;
+
+	public Bike() {
+		super();
+		this.handle="short";
+	}
+
+	public Bike(String handle) {
+		super();
+		this.handle = handle;
+	}
+
+	public String getHandle() {
+		return handle;
+	}
+	
+	
+
+}
